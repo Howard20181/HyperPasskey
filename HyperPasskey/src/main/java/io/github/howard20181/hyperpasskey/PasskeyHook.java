7166ad79c1801bab6b190284d7ccb34a4747331f
Howard Wu <HowardWu20@outlook.com>
2026-08-15 05:53:03 +0800

    Fix method name for security center application hook

--- HyperPasskey/src/main/java/io/github/howard20181/hyperpasskey/PasskeyHook.java
+++ HyperPasskey/src/main/java/io/github/howard20181/hyperpasskey/PasskeyHook.java
@@ -167,7 +167,7 @@ public class PasskeyHook extends XposedModule {
             }
             case securityCenterPackageName -> {
 try {
-    securityCenterApplicationHookE(classLoader, bridge);
+    securityCenterApplicationHook(classLoader, bridge);
 } catch (Exception e) {
                     log(Log.ERROR, TAG, "hook SecurityCenterApplication failed", e);
                 }
@@ -401,7 +401,7 @@ return true;
     }
 
 
-    private void securityCenterApplicationhookE(ClassLoader classLoader, DexKitCacheBridge.RecyclableBridge bridge) {
+    private void securityCenterApplicationhook(ClassLoader classLoader, DexKitCacheBridge.RecyclableBridge bridge) {
         var classDataList = new ArrayList<ClassData>();
         bridge.withBridge(rawBridge -> {
             var classData = rawBridge.getClassData("Lcom/miui/securitycenter/Application;");
